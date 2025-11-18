576a7f5aec5e20d38b67b74bd1b7423b76397a36
slackeddoodler <209493243+slackeddoodler@users.noreply.github.com>
2025-11-18 22:49:30 +0530

    Add files via upload

---
+++ groundx_client.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+from groundx import GroundX, Document
+from fastmcp import FastMCP
+
+load_dotenv()
+mcp = FastMCP("RAG-Agent")
+client = GroundX(api_key=os.getenv("GROUNDX_API_KEY"))
+
+buckets = client.buckets.list()
+
+bucket_id = buckets.buckets[0].bucket_id
+
+@mcp.tool()
+def search_doc_for_rag_context(query: str) -> str:
+    """
+    Searches and retrieves relevant context from a knowledge base,
+    based on the user's query.
+    Args:
+        query: The search query supplied by the user.
+    Returns:
+        str: Relevant text content that can be used by the LLM to answer the query.
+    """
+    response = client.search.content(
+        id=bucket_id,
+        query=query,
+        n=10,
+    )
+
+    return response.search.text
+
+@mcp.tool()
+def ingest_documents(local_file_path: str) -> str:
+    """
+    Ingest documents from a local file into the knowledge base.
+    Args:
+        local_file_path: The path to the local file containing the documents to ingest.
+    Returns:
+        str: A message indicating the documents have been ingested.
+    """
+    file_name = os.path.basename(local_file_path)
+    client.ingest(
+        documents=[
+            Document(
+            bucket_id=bucket_id,
+            file_name=file_name,
+            file_path=local_file_path,
+            file_type="pdf",
+            search_data=dict(
+            key = "value",
+        ),
+            )
+        ]
+    )
+    return f"""Ingested {file_name} into the knowledge base. 
+               It should be available in a few minutes"""
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
